df112271a37986ffe9c4158a301f06cb602d332a
Ty <lephuong29082002@gmail.com>
2026-06-05 19:57:39 +0700

    Day 01 Work with PyThon : Variables , F-String , BMI Caculator

---
+++ day01.py
@@ -0,0 +1,16 @@
+#Day 01 - Hello Python 
+# tao bien name  
+name = "Ty"
+# tao bien Tuoi ( age )
+age = 24 
+# in ra man hinh , f"..." cho phep nhung bien vao chuoi 
+print(f"Tên tôi là {name} và số tuổi của tôi là {age} tôi sẽ trở thành coder số 1 Thế giới và kiếm thật nhiều tiền vì cô ấy ( mẹ tôi )")  # lúc này name sẽ tự thay = "Ty" và age sẽ tự thay = 24 
+# cân nặng weight
+weight = 75
+# tạo biến height chứa chiều cao 
+height = 1.80
+# tính BMI  ở đây BMI là chỉ số mức độ béo gầy  của con người dựa trên cân nặng và chiều cao 
+BMI = weight/(height**2)
+# in kết quả 
+print(f" Chỉ số BMI của tôi là {BMI}")
+print(f"  Chỉ số BMI làm tròn 2 chữ số {BMI:.2f} ") # .2f là làm tròn 2 chữ số sau dấu phẩy
