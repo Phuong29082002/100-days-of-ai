4734742155ae6a6bb678e8790df7ba89b01741f6
Ty <lephuong29082002@gmail.com>
2026-06-05 20:13:54 +0700

    Day 02 PyThon If Else Elif

---
+++ day02.py
@@ -0,0 +1,18 @@
+age = 24 
+if age >= 18:
+    print ("Bạn đã có khởi đầu mới")
+else:
+    print ("Bạn chưa đến lúc bắt đầu")
+
+socre = 40
+if socre >= 90:
+    print ("Bạn xứng đáng với A")
+elif socre >=80:
+    print ("Bạn sấp đạt đến A rồi")
+elif socre >=70:
+    print ("Đừng dừng lại cố lên đang là B rồi ")
+elif socre >=50:
+    print ("Ai cũng có khởi đầu mà C rồi đừng nản ")
+else :
+    print ("Thất bại là mẹ thành công")
+    
